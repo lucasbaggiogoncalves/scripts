268e9f3885f023b2b0c3c8f8cc29aa5f7bda63e1
lucasbaggiogoncalves <lucasbaggiogoncalves@gmail.com>
2023-09-12 13:04:25 -0300

    Added script to export a specific tag value

---
+++ scripts/export-resourcegroup-resource-specific-tag.ps1
@@ -0,0 +1,40 @@
+Connect-AzAccount
+
+$results = @()
+
+$subscriptions = Get-AzSubscription
+foreach ($subscription in $subscriptions){
+    Set-AzContext -SubscriptionId $subscription.Id
+   
+    $resourcegroups = Get-AzResourceGroup
+    foreach ($resourcegroup in $resourcegroups){
+        if ($resourcegroup.Tags) {
+            $resourcegrouptags = $resourcegroup.Tags
+            $resourcegrouptagrateio = $resourcegrouptags['Rateio']
+        }
+        else {
+            $resourcegrouptagrateio = $null
+        }
+
+        $resources = Get-AzResource -ResourceGroupName $resourcegroup.ResourceGroupName
+        foreach ($resource in $resources) {
+            if ($resource.Tags) {
+                $resourcetags = $resource.Tags
+                $resourcetagrateio = $resourcetags['Rateio']
+            }
+            else {
+                $resourcetagrateio = $null
+            }
+
+            $results += [PSCustomObject] @{
+                SubscriptionName = $subscription.Name
+                ResourceGroup = $resourcegroup.ResourceGroupName
+                Resource = $resource.Name
+                ResourceId = $resource.Id
+                ResourceGroupTagRateio = $resourcegrouptagrateio
+                ResourceTagRateio = $resourcetagrateio
+                }
+        }
+    }
+}
+$results | Export-Csv -Path ".\resourcegroup-resource-tag.csv" -NoTypeInformation
