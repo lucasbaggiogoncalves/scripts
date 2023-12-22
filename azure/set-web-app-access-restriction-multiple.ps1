37ff1d8f83554a6883d174ebdf0aa5109c1c509c
lucasbaggiogoncalves <lucasbaggiogoncalves@gmail.com>
2023-12-22 14:19:43 -0300

    Modified script without resource group name

--- azure/set-web-app-access-restriction-multiple.ps1
+++ azure/set-web-app-access-restriction-multiple.ps1
@@ -1,32 +1,37 @@
 Connect-AzAccount
 
 # Resource variables
-$subscription = "subname-or-subid"
-$resourceGroupName = "rgname"
-$webAppName = "webappname"
-
-Set-AzContext -Subscription $subscription
+$subscription = "subname or id"
+$webAppNames = @(
+    "app1",
+    "app2"
+)
 
 # IPs variables
 $ipAddresses = @(
-    "value1",
-    "value2",
-    "value3",
+    "ip1",
+    "ip2"
 )
+
+Set-AzContext -Subscription $subscription
 
 # Initial priority (first rule)
 $initialPriority = 200
 
-# Loop through each IP
-foreach ($value in $ipAddresses) {
-    Add-AzWebAppAccessRestrictionRule -ResourceGroupName $resourceGroupName `
-                                      -WebAppName $webAppName `
-                                      -Priority $initialPriority `
-                                      -Action Allow `
-                                      -IpAddress $value
-                                      
-    Write-Output "Rule for the range $value created" 
+# Loop through each web app
+foreach ($webAppName in $webAppNames) {
+    $WebApps = Get-AzWebApp -Name $webAppName
 
-    # Increment the priority by 1 for each loop
-    $initialPriority++
+    foreach ($value in $ipAddresses) {
+        Add-AzWebAppAccessRestrictionRule -ResourceGroupName $WebApps.ResourceGroup `
+                                          -WebAppName $WebApps.Name `
+                                          -Priority $initialPriority `
+                                          -Action Allow `
+                                          -IpAddress $value
+                                          
+        Write-Output "Rule for the range $value created for web app $webAppName" 
+
+        # Increment the priority by 1 for each loop
+        $initialPriority++
+    }
 }
